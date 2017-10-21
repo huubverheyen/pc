a0d8b2ba8cb081b6b285be8720cabd96ae34ca09
Unknown <huub.verheyen@student.pxl.be>
2017-10-21 14:21:11 +0200

    h3

--- H6/src/be/pxl/h6/opdr3/H6opdr3.java
+++ H6/src/be/pxl/h6/opdr3/H6opdr3.java
@@ -5,19 +5,19 @@ public class H6opdr3 {
 
 	public static void main(String[] args) {
 		String text = "Hoi ik ben Bianca";
-//		text = text.toLowerCase();
-//		text = text.toUpperCase();
-//		text = text.replace( 'i', 'o');
-//		int tel =0;
-//		int pos = text.indexOf("e");
-//		while (pos != -1) {
-//			tel++;
-//			pos = text.indexOf("e", pos +1);
-//		}
-//		System.out.println(tel);
+		text = text.toLowerCase();
+		text = text.toUpperCase();
+		text = text.replace( 'i', 'o');
+		int tel =0;
+		int pos = text.indexOf("e");
+		while (pos != -1) {
+			tel++;
+			pos = text.indexOf("e", pos +1);
+		}
+		System.out.println(tel);
 		System.out.println(text);
-//		int length = text.length();
-//		System.out.println("De lengte van de tekst is " + length);
+		int length = text.length();
+		System.out.println("De lengte van de tekst is " + length);
 
 	}
 
